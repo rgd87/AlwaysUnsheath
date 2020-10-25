31ae5bc950cff4767819476f61c04abc4e9304dc
d87 <d87d87@gmail.com>
2020-10-25 16:25:27 +0700

    Added Interface Options, Renamed

--- unsheath.lua
+++ unsheath.lua
@@ -1,9 +1,40 @@
-local addonName, addonTable = ...
+local addonName, ns = ...
 
 local f = CreateFrame("Frame")
 f:SetScript("OnEvent", function(self, event, ...)
     return self[event](self, event, ...)
 end)
+
+local ticker
+local _, playerClass = UnitClass("player")
+local defaults = {
+}
+
+
+f:RegisterEvent("PLAYER_LOGIN")
+function f:PLAYER_LOGIN(event)
+    _G.AutoUnsheathDB = _G.AutoUnsheathDB or {}
+    ns.SetupDefaults(AutoUnsheathDB, defaults)
+
+    self:ReconfTicker()
+
+    local loader = CreateFrame('Frame', nil, InterfaceOptionsFrame)
+    loader:SetScript('OnShow', function(self)
+        self:SetScript('OnShow', nil)
+
+        if not f.optionsPanel then
+            f.optionsPanel = f:CreateGUI("AutoUnsheath")
+            InterfaceOptions_AddCategory(f.optionsPanel);
+        end
+    end)
+end
+
+f:RegisterEvent("PLAYER_LOGOUT")
+function f:PLAYER_LOGOUT(event)
+    ns.RemoveDefaults(AutoUnsheathDB, defaults)
+end
+
+
 
 local unsheath = function()
     if GetSheathState() ~= 2 then
@@ -11,33 +42,34 @@ local unsheath = function()
     end
 end
 
-local ticker
+
 
 f.MERCHANT_CLOSED = unsheath
 f.LOOT_CLOSED = unsheath
 
 
-function f:SPELLS_CHANGED(event)
-    local _, class = UnitClass("player")
+
+
+function f:ReconfTicker(event)
+    local db = AutoUnsheathDB
     local spec = GetSpecialization()
-    if class == "MONK" and spec == 1 then
+    local enabled = db[playerClass] and db[playerClass][spec]
+
+    if enabled then
         f:Enable()
     else
         f:Disable()
     end
-    -- if class == "DRUID" 
+    -- if class == "DRUID"
 end
 
-f.PLAYER_LOGIN = f.SPELLS_CHANGED
-
+f.SPELLS_CHANGED = f.ReconfTicker
 f:RegisterEvent("SPELLS_CHANGED")
-f:RegisterEvent("PLAYER_LOGIN")
 
 function f:Enable()
     -- self:RegisterEvent("PLAYER_ENTERING_WORLD")
-    -- self:RegisterUnitEvent("UNIT_AURA", "player")
     -- self:RegisterEvent("QUEST_ACCEPTED")
-    -- self:RegisterEvent("QUEST_FINISHED") 
+    -- self:RegisterEvent("QUEST_FINISHED")
     -- self:RegisterEvent("MERCHANT_CLOSED")
     self:RegisterEvent("LOOT_CLOSED")
     if not ticker then
@@ -52,4 +84,106 @@ function f:Disable()
         ticker:Cancel()
         ticker = nil
     end
+end
+
+
+local function MakeCheckbox(name, parent, icon, text)
+    local cb = CreateFrame("CheckButton", name, parent, "UICheckButtonTemplate")
+    cb:SetWidth(25)
+    cb:SetHeight(25)
+    cb:Show()
+
+    local cblabel = cb:CreateFontString(nil, "OVERLAY")
+    cblabel:SetFontObject("GameFontHighlight")
+    cblabel:SetPoint("LEFT", cb,"RIGHT", 5,0)
+    cblabel:SetText(text)
+    cb.label = cblabel
+
+    local cbtex = cb:CreateTexture(nil, "ARTWORK")
+    cbtex:SetTexture(icon)
+    cbtex:SetSize(25, 25)
+    cbtex:SetPoint("RIGHT", cb, "LEFT", -5, 0)
+    cb.icon = cbtex
+
+    return cb
+end
+
+function f:CreateGUI(name, parent)
+    local frame = CreateFrame("Frame", nil, InterfaceOptionsFrame)
+    frame:Hide()
+
+    frame.parent = parent
+    frame.name = name
+
+    frame:SetScript("OnShow", function(self)
+        local db = AutoUnsheathDB
+        for specIndex, cb in ipairs(self.content.specCheckBoxes) do
+            local enabled = db[playerClass] and db[playerClass][specIndex]
+            cb:SetChecked(enabled)
+        end
+    end)
+
+    local label = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+	label:SetPoint("TOPLEFT", 10, -15)
+	label:SetPoint("BOTTOMRIGHT", frame, "TOPRIGHT", 10, -45)
+	label:SetJustifyH("LEFT")
+    label:SetJustifyV("TOP")
+
+    label:SetText(name)
+
+	local content = CreateFrame("Frame", "CADOptionsContent", frame)
+	content:SetPoint("TOPLEFT", 10, -10)
+    content:SetPoint("BOTTOMRIGHT", -10, 10)
+
+    frame.content = content
+
+    content.specCheckBoxes = {}
+
+    for specIndex=1,GetNumSpecializations() do
+        local id, specName, description, icon = GetSpecializationInfo(specIndex)
+        local _, class = UnitClass('player')
+
+        local cb = MakeCheckbox(nil, content, icon, specName)
+        cb:SetPoint("TOPLEFT", 35, -10 - (specIndex*30))
+        cb.specID = specIndex
+        cb:SetScript("OnClick",function(self,button)
+            local db = AutoUnsheathDB
+            local specIndex = self.specID
+            db[playerClass] = db[playerClass] or {}
+            db[playerClass][specIndex] = not db[playerClass][specIndex]
+            f:ReconfTicker()
+        end)
+
+        table.insert(content.specCheckBoxes, cb)
+    end
+
+    return frame
+end
+
+
+function ns.SetupDefaults(t, defaults)
+    for k,v in pairs(defaults) do
+        if type(v) == "table" then
+            if t[k] == nil then
+                t[k] = CopyTable(v)
+            else
+                ns.SetupDefaults(t[k], v)
+            end
+        else
+            if t[k] == nil then t[k] = v end
+        end
+    end
+end
+function ns.RemoveDefaults(t, defaults)
+    for k, v in pairs(defaults) do
+        if type(t[k]) == 'table' and type(v) == 'table' then
+            ns.RemoveDefaults(t[k], v)
+            if next(t[k]) == nil then
+                t[k] = nil
+            end
+        elseif t[k] == v then
+            t[k] = nil
+        end
+    end
+    return t
 end
